436d9bf2d025238cf2a47e533cbe2264bee2c7c0
ooo <ooo@MICROSO-BPQN0MP>
2017-02-20 18:24:55 +0200

    java code

---
+++ Basic1/src/ua/study/Lesson1.java
@@ -0,0 +1,151 @@
+package ua.study;
+
+import javax.sound.midi.Synthesizer;
+
+public class Lesson1 {
+ 
+	public static void main(String[] args){
+	 
+	 //Comments
+	//Логічний тип данних	
+//	 	boolean b = true;
+//	 	
+//	 	
+//	 	System.out.println("boolian is value - "+b);
+//	 	
+//	 	b = false;
+//	 	
+//	 	System.out.println("boolian is value - "+b);
+//	 	
+//	 	/*
+//	 	 * 
+//	 	 */
+//	 	
+//	 	
+//	 	 byte s = 123;
+//	 	 short d = 30000;
+//	 	 int o = 354575;
+//	 	 long p = 1000000000999999999l;
+//	 	 
+//	 	 
+//	 	 float g = 2.345f;
+//	 	 double h = 5.7;
+//	 	 
+//	 	 char y = '%';
+//	 	 
+//	 	 String myName = "Oleg";
+//	 	 
+//	 	 System.out.println("my name"+ myName);
+//	 	 
+//	 	 System.out.println(3+2+ " value = " + (2+2));
+ 
+	
+	  /*int first = 10;
+	  int second = 5;
+	  
+	  if(first > second){
+		  System.out.println(first +  "bigger than" + second);
+	  }
+	  else{
+		  System.out.println();
+		  
+		  
+	  }*/
+	  
+	  
+//	  int one = 0;
+//	  
+//	  
+//	  if(one>0){
+//		  System.out.println(1);
+//		  }
+//	  else if(one<0){
+//		  System.out.println(2);
+//	  }
+//	  else if(one>100){
+//		  System.out.println(3);
+//	  }
+//	  else if(one==10){
+//		  System.out.println(4);
+//	  }
+//	  else{
+//		  System.out.println(5);
+//	  }
+		
+//		int key = 10;
+//		switch (key){
+//		case 1:{
+//			System.out.println("case 1");
+//			break;
+//		}
+//		case 2:{
+//			System.out.println("case 2");
+//			break;
+//		}
+//		case 3:{
+//			System.out.println("case 3"); 
+//			break;
+//		}
+//		default: {
+//			System.out.println("default");
+//			break;
+//		    }
+//		}
+		
+//		int hour = 10;
+//		switch (hour){
+//		case 9:{
+//			System.out.println("no");
+//		}
+//		case 10:{
+//			System.out.println("no");
+//		}
+//		case 11:{
+//			System.out.println("no");
+//		}
+//		case 12:{
+//			System.out.println("no");
+//		}
+//		case 13:{
+//			System.out.println("no");
+//			break;
+//		}
+//		case 14:{
+//			System.out.println("yes");
+//		}
+//		case 15:{
+//			System.out.println("yes");
+//		}
+//		case 16:{
+//			System.out.println("yes");
+//		}
+//		case 17:{
+//			System.out.println("yes");
+//		   }
+//		case 18:{
+//			}
+//		case 19:{
+//		}
+//		case 20:{
+//		}
+//		case 21:{
+//		}
+//		case 0:{
+//		}
+//		
+//		default:{
+//			System.out.println("nnn");
+//		  }
+//		}
+		
+		int hour = 21;
+		
+		if(hour>10 && hour>=21){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+	} 
+	
+}
